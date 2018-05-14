95b18a5d16a3df18c42434a92248ffe8d2bb80fe
alextremp <alex.castells@schibsted.com>
2018-05-14 14:30:21 +0200

    FIx the logger provider instantiation in Container
    * this fix a bootstrap error when initiating the connector in debug mode
    * it also adds the logger name to the log traces

--- src/openads-appnexus/AstClientImpl.js
+++ src/openads-appnexus/AstClientImpl.js
@@ -14,37 +14,37 @@ export default class AstClientImpl {
   }
 
   onEvent ({event, targetId, callback}) {
-    this._logger.debug('onEvent | event:', event, '| targetId:', targetId)
+    this._logger.debug(this._logger.name, '| onEvent | event:', event, '| targetId:', targetId)
     this._apnTag.anq.push(() => this._apnTag.onEvent(event, targetId, callback))
     return this
   }
 
   defineTag ({member, targetId, invCode, sizes, keywords, native}) {
-    this._logger.debug('defineTag | member:', member, '| targetId:', targetId, '| invCode:', invCode, '| sizes:', sizes, '| keywords:', keywords, '| native:', native)
+    this._logger.debug(this._logger.name, '| defineTag | member:', member, '| targetId:', targetId, '| invCode:', invCode, '| sizes:', sizes, '| keywords:', keywords, '| native:', native)
     this._apnTag.anq.push(() => this._apnTag.defineTag({member, targetId, invCode, sizes, keywords, native}))
     return this
   }
 
   loadTags () {
-    this._logger.debug('loadTags')
+    this._logger.debug(this._logger.name, '| loadTags')
     this._apnTag.anq.push(() => this._apnTag.loadTags())
     return this
   }
 
   showTag ({targetId}) {
-    this._logger.debug('showTag | targetId:', targetId)
+    this._logger.debug(this._logger.name, '| showTag | targetId:', targetId)
     this._apnTag.anq.push(() => this._apnTag.showTag(targetId))
     return this
   }
 
   refresh (targetsArray) {
-    this._logger.debug('refresh | targetsArray:', targetsArray)
+    this._logger.debug(this._logger.name, '| refresh | targetsArray:', targetsArray)
     this._apnTag.anq.push(() => this._apnTag.refresh(targetsArray))
     return this
   }
 
   modifyTag ({targetId, data}) {
-    this._logger.debug('modifyTag | targetId:', targetId, '| data:', data)
+    this._logger.debug(this._logger.name, '| modifyTag | targetId:', targetId, '| data:', data)
     this._apnTag.anq.push(() => this._apnTag.modifyTag(targetId, data))
     return this
   }
